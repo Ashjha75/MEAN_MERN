5770a533d51c1453362c791b5c9740680381f75c
Ashish jha <113693962+Ashjha75@users.noreply.github.com>
2023-07-02 12:05:53 +0530

    3rd test commit

--- NodeProject/Server.js
+++ NodeProject/Server.js
@@ -1,31 +1,36 @@
 let Htpp = require("http");
+const fs = require('fs');
+const Jsondata= fs.readFileSync('data.json','utf-8');
+
 let server= Htpp.createServer(function(req,res){
-     const html = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Node.js Server</title>
-      </head>
-      <body>
-        <h1>Hello, World!</h1>
-        <p>This is a basic Node.js server.</p>
-        <p>hello</p>
-        <img src="https://d1-slp-wp.supremelifeplatform.com/wp-content/backoffice_uploads/lodgement/2276/2023/56fab9b5bd7e9557a0ccd507c13e966d773ce4bc5037458c10.png" alt="Sample Image">      </body>
-    </html>
-  `;
+  const HtmlTemplate=fs.readFileSync('index.html','utf-8');
+  const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Node.js Server</title>
+    </head>
+    <body>
+      <h1>Hello, World!</h1>
+      <p>This is a basic Node.js server.</p>
+      <p>hello</p>
+      <img src="https://d1-slp-wp.supremelifeplatform.com/wp-content/backoffice_uploads/lodgement/2276/2023/56fab9b5bd7e9557a0ccd507c13e966d773ce4bc5037458c10.png" alt="Sample Image">      </body>
+  </html>
+`;
+
     if(req.url=='/'){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write("<html><body><p>This is first Url -Home Page</p></body></html>");
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.write(Jsondata);
         res.end();
     }
     else if(req.url=='/contact'){
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.write("<html><body><p>This is Second Url -Contact Page</p></body></html>")
+        res.write(HtmlTemplate)
         res.end();
     }
     else if(req.url=="/service"){
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(html);
+        res.write('html');
         res.end();
     }
     else{
